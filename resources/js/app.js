a01ae3bd054cbb3008d6065907a36ed5cab82e81
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-11-25 14:46:30 +0700

    add CRUD

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,46 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
-
+ 
 window.Vue = require('vue');
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app',
-});
+ 
+import VueRouter from 'vue-router';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+import App from './components/App.vue';
+import VueSweetalert2 from 'vue-sweetalert2'; 
+// If you don't need the styles, do not connect
+import 'sweetalert2/dist/sweetalert2.min.css';
+ 
+Vue.use(VueRouter);
+Vue.use(VueSweetalert2);
+Vue.use(VueAxios, axios);
+ 
+import ArticleIndex from './components/ArticleIndex.vue';
+import ArticleCreate from './components/ArticleCreate.vue';
+import ArticleShow from './components/ArticleShow.vue';
+import ArticleEdit from './components/ArticleEdit.vue';
+ 
+const routes = [
+  {
+      name: 'home',
+      path: '/',
+      component: ArticleIndex
+  },
+  {
+      name: 'create',
+      path: '/article/create',
+      component: ArticleCreate
+  },
+  {
+      name: 'edit',
+      path: '/article/edit/:id',
+      component: ArticleEdit
+  },
+  {
+    name: 'show',
+    path: '/article/show/:id',
+    component: ArticleShow
+}
+];
+ 
+const router = new VueRouter({ mode: 'history', routes: routes});
+const app = new Vue(Vue.util.extend({ router }, App)).$mount('#app');
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/articles', 'ArticleController@index');
+Route::post('/article/store', 'ArticleController@store');
+Route::get('/article/edit/{id}', 'ArticleController@getArticle');
+Route::get('/article/show/{id}', 'ArticleController@getArticle');
+Route::put('/article/update/{id}', 'ArticleController@update');
+Route::delete('/article/delete/{id}', 'ArticleController@delete'); 
